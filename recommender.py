9e2017ef17e7c5a67ac575c5b87f5aa89b4fa819
mtandon3 <34492359+mtandon3@users.noreply.github.com>
2018-11-11 22:55:52 -0700

    Update recommender.py
    adding import

--- recommender.py
+++ recommender.py
@@ -3,6 +3,7 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
+from scipy import stats
 
 # print(songs_summary.head()[['title', 'song_id']])
 
